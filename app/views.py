c0c3a8dc110617b9a592c128100d129a194fcf2f
luqqy <you@example.com>
2020-09-29 18:06:41 +0300

    updated the html

--- app/views.py
+++ app/views.py
@@ -8,4 +8,5 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    massage ='welcome to the blog world'
+    return render_template('index.html',massage=massage)
