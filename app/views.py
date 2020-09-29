ec48c7bafc602e925d5a43b492f2d282b881965b
luqqy <you@example.com>
2020-09-29 18:27:09 +0300

    updated index.html with new functionalities

--- app/views.py
+++ app/views.py
@@ -2,14 +2,14 @@ from flask import render_template
 from app import app
 
 # Views
-@app.route('/')
 def index():
 
     '''
     View root page function that returns the index page and its data
     '''
-    massage ='welcome to the blog world'
-    return render_template('index.html',massage=massage)
+
+    title = 'Home - Welcome to The World Of Blog'
+    return render_template('index.html', title = title)
 
 @app.route('/blog/<blog_id>')
 def movie(blog_id):
