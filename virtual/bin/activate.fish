dfacb86a57b9870d17ca3c4752650f170b656ea8
luqqy <you@example.com>
2020-09-29 18:00:28 +0300

    updated virtual environment

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/luqqy/Desktop/Blogg/virtual"
+set -gx VIRTUAL_ENV "/home/kebaso/Desktop/BLOG-APP/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
